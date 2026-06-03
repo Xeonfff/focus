22ac3892377141129fcf19bfe4884f2024b49e91
Xeonfff <Xeonfff@users.noreply.github.com>
2026-06-03 08:05:35 +0000

    deploy: e73cbf2665a1be64edbf1efb49222c72ec6ca0f8

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE = 'focus-v1';
+const URLS = ['/', 'index.html', 'manifest.json', 'icon.svg'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(URLS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).catch(() => r))
+  );
+});
